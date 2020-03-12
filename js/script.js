8ec488b0d3e842719a2dffdff850e5c37b9f255d
chrisofarabia <christopherallanperry@yahoo.co.uk>
2020-03-12 23:08:12 +0000

    Added new form fields, CSS, and JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const el = document.getElementById('register');
+el.addEventListener('click', registered, false);
 
+function registered(e) {
+  e.preventDefault();
+  alert('Your application has been registered');
+}
