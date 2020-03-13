caf348cfcbd2b212a2bdfbfb99c95f7762973bd1
chrisofarabia <christopherallanperry@yahoo.co.uk>
2020-03-13 21:58:50 +0000

    Added DOMContentLoaded event to script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-const el = document.getElementById('register');
-el.addEventListener('click', registered, false);
 
 function registered(e) {
   e.preventDefault();
   alert('Your application has been registered');
 }
+
+function init() {
+  const el = document.getElementById('register');
+  el.addEventListener('click', registered, false);
+}
+
+document.addEventListener('DOMContentLoaded', init, false);
